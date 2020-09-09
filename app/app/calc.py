d9ae3fcecd874f429879a99a3d3d8ec71578ae14
rcocariu <roxanacocariu@yahoo.com>
2020-09-08 22:28:38 -0400

    reformat

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,6 +2,7 @@ def add(x, y):
     """Add two numbers together and return the result"""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """subtracts two numbers """
     return y - x
