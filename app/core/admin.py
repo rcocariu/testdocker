20f507da2a34c21b230c39709463878bc73ef592
rcocariu <roxanacocariu@yahoo.com>
2020-11-27 19:51:40 -0500

    latest

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-"""conbert strings into human readable text"""
+"""convert strings into human readable text"""
 from django.utils.translation import gettext as _
 
 from core import models
